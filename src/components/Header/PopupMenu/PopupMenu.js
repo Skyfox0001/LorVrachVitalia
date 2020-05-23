f7ff08c49d9a3fd8e1d9b53f620f1642ef0aec72
vega <S.greblov@gmail.com>
2020-05-22 18:43:49 -0700

    Finished menu

--- src/components/Header/PopupMenu/PopupMenu.js
+++ src/components/Header/PopupMenu/PopupMenu.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./PopupMenu.module.scss"
 
 const PopupMenu = ({isOpen}) => {
+    const [state, setState] = useState({
+        list : [
+            //{link: "link", name: "Главная"},
+            {link: "link", name: "Вопросы"},
+            {link: "link", name: "Статьи"},
+            {link: "link", name: "Регистрация"},
+        ],
+        contacts:
+            {link: "link", name: "Контакты"},
 
+    });
     const popupMenuCls = [classes.PopupMenu];
 
     if (isOpen) {
@@ -10,9 +20,22 @@ const PopupMenu = ({isOpen}) => {
     }
 
 
+    let menu  = state.list.map((element, index)=>{
+        return(
+            <div className={classes.element} key={index}>
+                <a href={element.link}>{element.name}</a>
+            </div>
+        )
+    })
     return (
         <div className={popupMenuCls.join(" ")}>
-
+            <div className={classes.menuLinks}>
+                {menu}
+                <div className={classes.line}></div>
+                <div className={classes.contacts}>
+                    <p>{state.contacts.name}</p>
+                </div>
+            </div>
         </div>
     );
 };
